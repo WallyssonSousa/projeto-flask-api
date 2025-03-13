743648502e64fd8c7c0f4f42f80d395ddfb6b6f8
Nicolas Dias Reis <nico.reisdias@gmail.com>
2025-03-13 20:20:17 -0300

    Inicio do desenvolvimento pela gravação

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
-print("Hello, Wold!")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+dici = {
+    "professores":[
+        {   "id" : 1,
+            "nome" : "Caio Ireno",
+            "idade" : 27,
+            "materia" : "String",
+            "observacoes" : "String"
+            }
+    ]
+}
+
+@app.route('/professores', methods=['GET'])
+def getProfessor():
+    dados = dici["professores"] 
+    return jsonify(dados)
+
+@app.route('/professores', methods=['POST'])
+def createProfessor():
+    dados = request.json
+    professor = dici['professores']
+    professor.append(dados)
+    return jsonify(dados)
+
+@app.route('/professores/<int:idProfessor>', methods=['PUT'])
+def updateProfessor(idProfessor):
+    professores = dici['professores']
+    for professor in professores:
+        if professor['id'] == idProfessor:
+            r = request.json
+            professor['nome'] = r['nome']
+            return jsonify(r),201
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
