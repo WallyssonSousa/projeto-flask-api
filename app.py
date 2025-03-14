4e2b3e5b95434096df9ddf5cfd95a9c7aa21cf73
Nicolas Dias Reis <nico.reisdias@gmail.com>
2025-03-13 23:00:41 -0300

    GET e POST funcionando

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask, jsonify, request
+from datetime import datetime
 
 app = Flask(__name__)
 
-dici = {"professores":[{ }]}
+dici = {"professores":[]}
 
 id_professor = 1
 
-@app.route('/professores', methods=['POST'])
 
 def validar_campos_obrigatorios(dados, campos_obrigatorios):
-    if not campos_obrigatorios.isubset(dados.keys()):
+    if not campos_obrigatorios.issubset(dados.keys()):
         return {"erro": "Faltam campos obrigatórios!"}, 400 #Verificação = campos obrigatório faltando
     if set(dados.keys()) != campos_obrigatorios:
         return {"erro": "Campos inválidos no JSON!"}, 400 # Verificação = os campos
                                                           # são diferentes dos obrigatórios ou a mais
     return None, 200 #verificação = OK
 
+def calcular_idade(data_nascimento):
+    today = datetime.today()
+    nascimento = datetime.strptime(data_nascimento, "%Y-%m-%d")
+    idade = today.year - nascimento.year -((today.month, today.day)<(nascimento.month, nascimento.day))
+    return idade
+
+
+@app.route('/professores', methods=['POST'])
 def createProfessor():
     global id_professor
     dados = request.json
@@ -25,16 +33,23 @@ def createProfessor():
     if erro:
         return jsonify(erro), status
     
+    dados["idade"] = calcular_idade(dados["data_nascimento"])
+
+    dados["id"] = id_professor
+    
     for professor in dici["professores"]:
         if professor["id"] == dados["id"]:
             return jsonify({"erro": "ID de professor já está cadastrado!"}), 400
         
-    dados["id"] = id_professor
-
     id_professor += 1
 
     dici["professores"].append(dados)
+    
     return jsonify(dados), 201
+
+@app.route('/professores', methods=['GET'])
+def getProfessores():
+    return jsonify(dici["professores"])
 
 
 if __name__ == "__main__":
