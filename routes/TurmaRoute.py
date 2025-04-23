891e268e5b46b7e55504344aa4efe2cca224b275
Nicolas Dias Reis <nico.reisdias@gmail.com>
2025-04-22 22:07:35 -0300

    Update route Turmas_post

--- routes/TurmaRoute.py
+++ routes/TurmaRoute.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from models.TurmaModel import turmas_get, turma_get_id, turma_post, turma_PUT, turma_DELETE
 from flask_jwt_extended import jwt_required, get_jwt
+from utils.FucoesValidacao import validar_campos_obrigatorios
 
 turma_bp = Blueprint('turma_bp', __name__)
 
@@ -33,15 +34,12 @@ def create_turma():
     if user.get("role") != "admin":
         return jsonify({"erro": "Acesso negado: apenas administradores podem criar novas turmas"}), 403
 
-    try:
-        nova_turma = request.get_json()
-        if not nova_turma:
-            return jsonify({"erro": "Dados ausentes ou inválidos"}), 400
-
-        turma = turma_post(nova_turma)
-        return jsonify(turma), 201
-    except Exception as e:
-        return jsonify({"erro": f"Erro ao criar turma: {str(e)}"}), 500
+    nova_turma = request.json
+    campos_obrigatorios = {"nome", "turno", "professor_id"}
+    
+    erro, status = validar_campos_obrigatorios(nova_turma, campos_obrigatorios)
+    if erro:
+        return jsonify(erro), status
 
 # Rota para atualizar uma turma existente
 @turma_bp.route('/turmas/<int:turma_id>', methods=['PUT'])
