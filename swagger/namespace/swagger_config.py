dcd24709e9fb446a57c9b9cae1088715b01b0f22
Nicolas Dias Reis <nico.reisdias@gmail.com>
2025-04-30 21:21:17 -0300

    Update folders and imports

--- swagger/namespace/swagger_config.py
+++ swagger/namespace/swagger_config.py
@@ -1,9 +1,9 @@
-from swagger.namespaces.aluno_namespace import alunos_ns
+from swagger.namespace import alunos_ns
 
 # Função para registrar os namespaces
 def configure_swagger(app):
-    api.init_app(app)
-    api.add_namespace(alunos_ns, path="/alunos")
-    api.add_namespace(professores_ns, path="/professores")
-    api.add_namespace(turmas_ns, path="/turmas")
-    api.mask_swagger = False
+    app.init_app(app)
+    app.add_namespace(alunos_ns, path="/alunos")
+    #app.add_namespace(professores_ns, path="/professores")# implentar ainda o namespace professores
+    #app.add_namespace(turmas_ns, path="/turmas")# implementar ainda o namespace turmas
+    app.mask_swagger = False
